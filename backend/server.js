69a13bc24306cceac23bd2267c3d8a7baf30ed52
Charles Cheng <charles.yhcheng@gmail.com>
2022-03-24 23:51:38 -0700

    Setup router and controller in backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4000
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.status(200).json({message: "Hello there from express"})
+})
+
+// Routes
+app.use('/api/users',require("./routes/userRoutes"))
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
