7d000b57632430828bb13aead2c3bcbfe116ec08
Charles Cheng <charles.yhcheng@gmail.com>
2022-03-28 21:44:12 -0700

    Connect Register and Login function with global states via redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
@@ -8,16 +10,22 @@ function Login() {
   });
   const { email, password } = formData;
 
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
   const handleChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    // if (password !== password2) {
-    //   console.log("Passwords do not match");
-    //   toast.error("Passwords do not match");
-    //   return;
-    // }
+    const userData = {
+      email,
+      password
+    }
+    dispatch(login(userData))
     console.log("Form data submitted");
   };
   return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
 function Register() {
   const [formData, setFormData] = useState({
     name: "",
@@ -10,6 +12,12 @@ function Register() {
   });
   const { name, email, password, password2 } = formData;
 
+  const dispatch = useDispatch();
+
+  const { user, isSuccess, isLoading, message  } = useSelector(
+    (state) => state.auth
+  );
+
   const handleChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -18,7 +26,14 @@ function Register() {
     if (password !== password2) {
       console.log("Passwords do not match");
       toast.error("Passwords do not match");
-      return
+      return;
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+      dispatch(register(userData))
     }
     console.log("Form data submitted");
   };
