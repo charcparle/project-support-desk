f6a370fa65835fb1cee76ca2fbeb790e64915186
Charles Cheng <charles.yhcheng@gmail.com>
2022-04-01 00:24:17 -0700

    Connect creatTicket function via Redux

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { defaultEqualityCheck } from "reselect";
+
+const API_URL = "/api/tickets";
+
+// Create new ticket
+const createTicket = async (ticketData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.post(API_URL, ticketData, config);
+  return response.data;
+};
+
+const ticketService = { createTicket };
+
+export default ticketService;
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
 
 function NewTicket() {
   const { user } = useSelector((state) => state.auth);
+  const { isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.ticket
+  );
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [product, setProduct] = useState("Signalling");
   const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess) {
+      dispatch(reset());
+      navigate("/tickets");
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, message, dispatch, navigate]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const ticketData = {
+      product,
+      description,
+    };
+    dispatch(createTicket(ticketData));
     console.log("Form data submitted");
   };
 
@@ -56,7 +80,7 @@ function NewTicket() {
             ></textarea>
           </div>
           <div className="form-group">
-              <button className="btn btn-block">Submit</button>
+            <button className="btn btn-block">Submit</button>
           </div>
         </form>
       </section>
